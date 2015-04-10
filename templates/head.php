b2992c973fc6dbb071367964b33fbe5ae1f95092
Adam Simms <adam@trello.com>
2015-04-10 20:09:56 -0230

    Added Typekit

--- templates/head.php
+++ templates/head.php
@@ -6,4 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
     <?php wp_head(); ?>
+
+    <!-- Typekit -->
+    <script type="text/javascript" src="//use.typekit.net/zua1yrw.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   </head>
